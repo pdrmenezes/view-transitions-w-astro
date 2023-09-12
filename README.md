d4364e8609969919a2cc10c8450a0d9bac1b1aad
pdrmenezes <pdrmenezes1@gmail.com>
2023-09-12 15:48:09 -0300

    feat:city specific page

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Astro + View Transitions + Container Queries in TailwindCSS
 
 > Inspired by Jack Herrington's video
+
+- Using Container queries to make our only `Card.astro` Component adapt to whatever container size its in
+  - We can define the container that will be the reference to its children by adding the `@container` class
+  - just by adding `@` before the media query we can use it to specify that the size is relative to its parent container
---
+++ src/data/cities.ts
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: "brasilia",
+    name: "Brasília",
+    src: "/brasilia.png",
+  },
+  {
+    id: "cidade-de-goias",
+    name: "Cidade de Goiás",
+    src: "/cidade-de-goias.png",
+  },
+  {
+    id: "diamantina",
+    name: "Diamantina",
+    src: "/diamantina.png",
+  },
+  {
+    id: "fortaleza",
+    name: "Fortaleza",
+    src: "/fortaleza.png",
+  },
+  {
+    id: "recife",
+    name: "Recife",
+    src: "/recife.png",
+  },
+  {
+    id: "san-sebastian-xolalpa",
+    name: "San Sebastian Xolalpa",
+    src: "/san-sebastian.jpg",
+  },
+  {
+    id: "sao-paulo",
+    name: "São Paulo",
+    src: "/sao-paulo.png",
+  },
+];
